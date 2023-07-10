f350f1c3a1720e0e5bcfa3d1e6dee4caf00422cf
Paul Barrass <paul.barrass@gmail.com>
2023-07-10 19:11:49 +0100

    24 balls

--- clump/src/App.js
+++ clump/src/App.js
@@ -8,7 +8,7 @@ const rfs = THREE.MathUtils.randFloatSpread
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
 const baubleMaterial = new THREE.MeshStandardMaterial({ color: "white", roughness: 0, envMapIntensity: 1 })
 
-const numBalls = 10
+const numBalls = 24
 export const App = () => (
   <Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}>
     <ambientLight intensity={0.5} />
@@ -30,7 +30,7 @@ function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props 
   const texture = useTexture("./at.png")
   const [ref, api] = useSphere(() => ({ args: [1], mass: 1, angularDamping: 0.1, linearDamping: 0.65, position: [rfs(20), rfs(20), rfs(20)] }))
   useFrame((state) => {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < numBalls; i++) {
       // Get current whereabouts of the instanced sphere
       ref.current.getMatrixAt(i, mat)
       // Normalize the position and multiply by a negative force.
@@ -38,7 +38,17 @@ function Clump({ mat = new THREE.Matrix4(), vec = new THREE.Vector3(), ...props 
       api.at(i).applyForce(vec.setFromMatrixPosition(mat).normalize().multiplyScalar(-40).toArray(), [0, 0, 0])
     }
   })
-  return <instancedMesh ref={ref} castShadow receiveShadow args={[null, null, 10]} geometry={sphereGeometry} material={baubleMaterial} material-map={texture} />
+  return (
+    <instancedMesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      args={[null, null, numBalls]}
+      geometry={sphereGeometry}
+      material={baubleMaterial}
+      material-map={texture}
+    />
+  )
 }
 
 function Pointer() {
